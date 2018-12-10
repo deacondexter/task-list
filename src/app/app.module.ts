aabd2a34cf37943201f6da9285696279f6bfc886
APL <ap_leary@hotmail.com>
2018-12-10 02:27:37 -0500

    workshop 2nd attempt

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskAddComponent } from './task-add/task-add.component';
+import { NavComponent } from './nav/nav.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+
+@NgModule({
+  declarations: [AppComponent, TaskListComponent, TaskAddComponent, NavComponent, TaskEditComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
